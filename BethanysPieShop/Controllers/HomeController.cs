0443f7826eb76bcacdddefa49e6a8407a7e60c58
Juan Renteria <jrenteria.camino@gmail.com>
2022-11-16 14:51:03 -0800

    Added Home Controller

---
+++ BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using BethanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var pieOfTheWeek = _pieRepository.PieOfTheWeek;
+
+            var homeViewModel = new HomeViewModel(pieOfTheWeek);
+            return View(homeViewModel);
+        }
+    }
+}
