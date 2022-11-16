ce0b3d5d3970277987696c610600f0b8b7ea6c24
Juan Renteria <jrenteria.camino@gmail.com>
2022-11-15 17:12:13 -0800

    Added services configurations to Program.cs

--- BethanysPieShop/Program.cs
+++ BethanysPieShop/Program.cs
@@ -1,6 +1,29 @@
+using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
+
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration["ConnectionString:BethanysPieShopDbContextConnection"]);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseStaticFiles();
+app.MapDefaultControllerRoute();
+
+//DBInitializer.Seed(app);
 
 app.Run();
