70b10cf33b554cfe155fdc546212af0e39c21546
Juan Renteria <jrenteria.camino@gmail.com>
2022-11-15 17:00:12 -0800

    Added constructor and List method to Pie Controller

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -1,9 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BethanysPieShop.Controllers
 {
     public class PieController : Controller
     {
-       
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            PieListViewModel pieListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cake");
+            return View(pieListViewModel);
+        }
     }
 }
