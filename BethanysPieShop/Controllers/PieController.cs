4697c5b3d8d0932708cf4e2abf432d5ae5e53115
Juan Renteria <jrenteria.camino@gmail.com>
2022-11-16 17:32:37 -0800

    Added Home andpies link to navigation bar

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -23,11 +23,12 @@ namespace BethanysPieShop.Controllers
         public IActionResult Deails(int id)
         {
             var pie = _pieRepository.GetPieById(id);
-            if(pie == null)
+            if (pie == null)
             {
                 return NotFound();
             }
             return View(pie);
+        }
 
     }
 }
