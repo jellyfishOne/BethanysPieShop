08f381ba4fc8934d7754fa58d5e5a2e9ab1f602d
Juan Renteria <jrenteria.camino@gmail.com>
2022-12-03 10:09:00 -0800

    Added Pie Controller Test class

---
+++ BethaysPieShopTests/Controllers/PieControllerTests.cs
@@ -0,0 +1,19 @@
+﻿using BethanysPieShop.Controllers;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethaysPieShopTests.Controllers
+{
+    public class PieControllerTests
+    {
+        [Fact]
+        public void List_EmptyCategory_ReturnsAllPies()
+        {
+        }
+    }
+}
